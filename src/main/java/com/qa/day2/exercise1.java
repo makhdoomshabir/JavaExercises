e83ecd118a3e6c89b7cfcc0185e58c96b90e0e4f
Makhdoom Shabir <MShabir@qa.com>
2020-08-07 20:19:35 +0100

    created two integers and their summation

--- src/main/java/com/qa/day2/exercise1.java
+++ src/main/java/com/qa/day2/exercise1.java
@@ -1,4 +1,12 @@
 package com.qa.day2;
 
 public class exercise1 {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+
 }
