eb8c9f5644fadb481efd8df7e48a3f41ee83fa67
Makhdoom Shabir <MShabir@qa.com>
2020-08-07 20:22:14 +0100

    made third integer and divided int3/int2

--- src/main/java/com/qa/day2/exercise1.java
+++ src/main/java/com/qa/day2/exercise1.java
@@ -5,10 +5,13 @@ public class exercise1 {
     public static void main(String[] args) {
         int num1 = 3;
         int num2 = 5;
+        int num3 = 20;
         int result1 = num1 + num2;
         int result2 = num1 * num2;
+        int result3 = num3 / num2;
         System.out.println(result1);
         System.out.println(result2);
+        System.out.println(result3);
 
 
     }
