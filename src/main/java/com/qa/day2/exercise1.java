b747a7eecbd6ccf4b4859bb6b13c559b0b8cbcd9
Makhdoom Shabir <MShabir@qa.com>
2020-08-07 21:02:13 +0100

    renamed class and made new method for the integer sums

--- src/main/java/com/qa/day2/exercise1.java
+++ src/main/java/com/qa/day2/exercise1.java
@@ -3,17 +3,20 @@ package com.qa.day2;
 public class exercise1 {
 
     public static void main(String[] args) {
-        int num1 = 3;
-        int num2 = 4;
-        int num3 = 20;
-        int result1 = num1 + num2;
-        int result2 = num1 * num2;
-        int result3 = num3 / num2;
-        System.out.println(result1);
-        System.out.println(result2);
-        System.out.println(result3);
-
+        resultsThreeIntegers();
 
     }
 
+        public static void resultsThreeIntegers (){
+            int num1 = 3;
+            int num2 = 4;
+            int num3 = 20;
+            int result1 = num1 + num2;
+            int result2 = num1 * num2;
+            int result3 = num3 / num2;
+            System.out.println(result1);
+            System.out.println(result2);
+            System.out.println(result3);
+
+        }
 }
--- src/main/java/com/qa/day2/exercise2.java
+++ src/main/java/com/qa/day2/exercise2.java
@@ -2,6 +2,11 @@ package com.qa.day2;
 
 public class exercise2 {
     public static void main(String[] args) {
+        class ResultsClass {
+
+
+        }
 
     }
+
 }
