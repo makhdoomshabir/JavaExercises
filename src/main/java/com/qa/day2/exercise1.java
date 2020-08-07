0a4518eae8b2aebd1c6406b5aa162d1bf5d98837
Makhdoom Shabir <MShabir@qa.com>
2020-08-07 20:20:48 +0100

    found the multiple sum of two ints

--- src/main/java/com/qa/day2/exercise1.java
+++ src/main/java/com/qa/day2/exercise1.java
@@ -5,8 +5,12 @@ public class exercise1 {
     public static void main(String[] args) {
         int num1 = 3;
         int num2 = 5;
-        int result = num1 + num2;
-        System.out.println(result);
+        int result1 = num1 + num2;
+        int result2 = num1 * num2;
+        System.out.println(result1);
+        System.out.println(result2);
+
+
     }
 
 }
