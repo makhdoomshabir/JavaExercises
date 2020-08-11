6d946e7d6ba75ae17ecf9bf6374eb1a7f886fab5
Makhdoom Shabir <MShabir@qa.com>
2020-08-11 14:50:30 +0100

    created numbers and boolean

--- src/main/java/com/qa/day3/FlowCharts.java
+++ src/main/java/com/qa/day3/FlowCharts.java
@@ -1,6 +1,9 @@
 package com.qa.day3;
 
 public class FlowCharts {
+    public int num1 = 4;
+    public int num2 = 9;
+    public boolean numberType = true;
 
     public static void main(String[] args) {
 
