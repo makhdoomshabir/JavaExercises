bcd5c4333a97878b0ca58e267f1bb524e5fd45a9
Makhdoom Shabir <MShabir@qa.com>
2020-08-11 14:58:38 +0100

    made boolean static

--- src/main/java/com/qa/day3/FlowCharts.java
+++ src/main/java/com/qa/day3/FlowCharts.java
@@ -1,11 +1,14 @@
 package com.qa.day3;
 
 public class FlowCharts {
-    public int num1 = 4;
-    public int num2 = 9;
-    public boolean numberType = true;
+
+    public static boolean numberType = true;
 
     public static void main(String[] args) {
-
+        if(numberType) {
+            System.out.println("correct");
+        }else {
+            System.out.println("false");
+        }
     }
 }
