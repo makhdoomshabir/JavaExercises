406eecc802b3aae25cd9e241f260fd8347d2d5df
Makhdoom Shabir <MShabir@qa.com>
2020-08-11 21:22:25 +0100

    added numbers module to main string to correctly allow code to run. added result1,2

--- src/main/java/com/qa/day3/FlowCharts.java
+++ src/main/java/com/qa/day3/FlowCharts.java
@@ -1,14 +1,23 @@
 package com.qa.day3;
 
 public class FlowCharts {
-
-    public static boolean numberType = true;
+    public static int num1 = 6;
+    public static int num2 = 9;
+    public static boolean numberType = !true;
 
     public static void main(String[] args) {
-        if(numberType) {
-            System.out.println("correct");
-        }else {
-            System.out.println("false");
+        numbers();
+    }
+    public static int numbers(){
+        int result1 = num1 +num2;
+        int result2 = num1 * num2;
+
+            if(numberType) {
+                System.out.println(result1);
+                return(result1);
+        }   else {
+                System.out.println(result2);
+                return(result2);
         }
     }
 }
