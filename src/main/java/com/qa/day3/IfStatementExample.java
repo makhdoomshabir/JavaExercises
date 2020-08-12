9fa2f3f0f375d413446456ebf3a6daf4fb82d074
Makhdoom Shabir <MShabir@qa.com>
2020-08-12 13:34:49 +0100

    peer programme creating 100 positions in a race scenario

---
+++ src/main/java/com/qa/day3/IfStatementExample.java
@@ -0,0 +1,16 @@
+package com.qa.day3;
+
+public class IfStatementExample {
+
+        public static void main(String[] args) {
+            for(int i = 0; i < 10; i++) {
+                if(i == 2) {
+                    continue;
+                }
+                if(i == 7) {
+                    break;
+                }
+                System.out.println(i);
+            }
+        }
+    }
